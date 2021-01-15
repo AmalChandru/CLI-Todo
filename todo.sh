fc51f3278ffcd3a62bc81cac6959826ce88dccf6
AmalChandru <amaltathva@gmail.com>
2021-01-15 20:21:26 +0530

    created-todo,task

---
+++ todo.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "--------------Hey there, Welcome to CLI Todo list-------------"
+echo " "
+echo " "
+echo "              1.Press 1, then 'Enter' to add a new task                      "
+echo "              2.Press 2, then 'Enter' to remove completed task               "
+echo "              3.Press 3, then 'Enter' to get list of all tasks               "
+echo "              4.Press 4, then 'Enter' to remove all tasks                    "
+echo " "
+echo " "
+echo "---------------------------------------------------------------"
+
+read -s varChoice
+
+if [ $varChoice == 1 ]
+then
+cat task.txt
+fi
+if [ $varChoice == 2 ]
+then
+cat task.txt
+fi
+if [ $varChoice == 3 ]
+then
+cat task.txt
+fi
+if [ $varChoice == 4 ]
+then
+cat task.txt
+fi
